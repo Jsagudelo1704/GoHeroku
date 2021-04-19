d317062aaf532a02488461d4c832435e0b97b2b5
Jsagudelo1704 <82290762+Jsagudelo1704@users.noreply.github.com>
2021-04-18 21:03:23 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+/*
 package main
 
 import (
@@ -25,4 +26,34 @@ func main() {
 }
 func Version(context echo.Context) error {
 	return context.JSON(http.StatusOK, map[string]interface{}{"version": 2})
+}
+*/
+
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Jsagudelo1704/Go/services"
+)
+
+const rutabase = "/api"
+
+func main() {
+
+	services.SetupRoutes(rutabase)
+	err := http.ListenAndServe(GetPort(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	if port == "" {
+		port = "4747"
+	}
+	return ":" + port
 }
---
+++ services.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const rutaMutant = "mutant/"
+const rutaStats = "stats"
+
+//Invocar controlador para servicio Mutant
+func handleMutant(w http.ResponseWriter, r *http.Request) {
+	context.JSON(http.StatusOK, map[string]interface{}{"ruta": "mutatn"})
+	//mutantController(w, r)
+}
+
+//Invocar controlador para servicio Stats
+func handleStats(w http.ResponseWriter, r *http.Request) {
+	context.JSON(http.StatusOK, map[string]interface{}{"ruta": "stats"})
+	//statsController(w, r)
+}
+
+// SetupRoutes :
+func SetupRoutes(rutabase string) {
+	mutantHandler := http.HandlerFunc(handleMutant)
+	statsHandler := http.HandlerFunc(handleStats)
+	http.Handle(fmt.Sprintf("%s/%s", rutabase, rutaMutant), mutantHandler)
+	http.Handle(fmt.Sprintf("%s/%s", rutabase, rutaStats), statsHandler)
+}
